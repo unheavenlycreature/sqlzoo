b8ef769f95b7f04f88ce5163eb58b27c07324600
unheavenlycreature <imaginaryenemy@protonmail.com>
2020-02-05 11:28:58 -0500

    Add select basics solutions.

---
+++ 0_select_basics.sql
@@ -0,0 +1,27 @@
+/* Exercises at https://sqlzoo.net/wiki/SELECT_basics */
+/* Exercise 1 */
+SELECT
+  population
+FROM
+  world
+WHERE
+  name = 'Germany';
+
+/* Exercise 2 */
+SELECT
+  name,
+  population
+FROM
+  world
+WHERE
+  name IN ('Sweden', 'Norway', 'Denmark');
+
+/* Exercise 3 */
+SELECT
+  name,
+  area
+FROM
+  world
+WHERE
+  area BETWEEN 200000
+  AND 250000
